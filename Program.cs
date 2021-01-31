34b0a3dd6cfc7e697b6946799ec7bd96613e8aff
Leonardo Dimarchi <leonardodimarchi@users.noreply.github.com>
2021-01-31 15:08:31 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace firstDotnetConsoleapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 5;
+
+            for(int i=1; i <= numero; i++){
+                Console.WriteLine("Hello World {0}", i);
+            }
+        }
+    }
+}
